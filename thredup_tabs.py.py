c66b89c638b1197b063ab9e6e763bccef274abcd
tas09009 <taniya.singh12@gmail.com>
2019-09-12 08:48:26 -0400

    Add files via upload

--- thredup_tabs.py.py
+++ thredup_tabs.py.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 
 
 # Set the URL that you want to webscrape from
-url = 'https://www.thredup.com/products/petite?chars_sleeve_length=sleeveless&department_tags=petite&search_tags=women-tops%2Cwomen-tops-blouses&sizing_id=750%2C755&skip_equivalents=true&sort=Newest+First&state=listed'
+url = 'https://www.thredup.com/products/petite?chars_sleeve_length=sleeveless&department_tags=petite&search_tags=women-tops%2Cwomen-tops-blouses&sizing_id=750%2C755&skip_equivalents=true&state=listed'
 
 # Connect to URL
 response = requests.get(url)
@@ -21,20 +21,30 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser") # <class 'bs4.BeautifulSoup'>
 # print(soup.prettify()) # class 'str'
 
+product = soup.find_all(attrs={"class": "results-grid-item"})
+# [<div data-foo="value">foo!</div>]
+
+print(product)
+
+
+
 
 
 # for link in soup.find_all('a'):
-#     link.get('href')
-#     print(type(link)) # <class 'bs4.element.Tag'>
+#     links = link.get('href')
+# download_url = 'https://www.thredup.com/' + links # combine the links to pull data from
+# print(download_url) # <class 'str'>
 
 
 
-for i in range(1,):
-	one_a_tag = soup.findAll('a')[i] # find all the 'a' tags
-	link = one_a_tag['href'] # pull all 'href' data within 'a' tags
-	list_link = list(link)
-	download_url = 'https://www.thredup.com/' + link # combine the links to pull data from
-	print(download_url)
+# one_a_tag = soup.findAll('a') # find all the 'a' tags
+# link = one_a_tag['href'] # pull all 'href' data within 'a' tags
+# # list_link = list(link)
+# download_url = 'https://www.thredup.com/' + link # combine the links to pull data from
+# print(download_url) # <class 'str'>
+
+
+	
 # download_url = 'https://www.thredup.com/' + link # combine the links to pull data from
 
 
